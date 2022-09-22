f4e45c02c95223e1cf412dbe46c06c565ac04c95
star <y1486170457@qq.com>
2022-09-22 15:06:24 +0800

    Update 4.5_快速幂.cpp

--- 知识点精讲/4.5_快速幂.cpp
+++ 知识点精讲/4.5_快速幂.cpp
@@ -1,41 +1,47 @@
-//��pow(a,b)%m
+//求pow(a,b)%m
 
-/*���ڶ���˼�루Ҳ�ж����ݣ�
-1.���b��������pow(a,b) = a*pow(a,b-1);
-2.���b��ż����pow(a,b) = pow(a,b/2)*pow(a,b/2);
+/*基于二分思想（也叫二分幂）
+快速幂，二进制取幂（Binary Exponentiation，也称平方法），是一个在logn的时间内计算a的n次方的小技巧，而暴力的计算需要o(n)的时间。
+1.如果b是奇数，pow(a,b) = a*pow(a,b-1);
+2.如果b是偶数，pow(a,b) = pow(a,b/2)*pow(a,b/2);
 */
 
-//�����ݵĵݹ�д��
+//快速幂的递归写法
 typedef long long LL;
 LL binaryPow(LL a,LL b,LL m){
     if(b==0) return 1;
-    if(b&1) return a*binaryPow(a,b-1,m)%m;//  if(b&1)ִ���ٶȿ�һ��
+    if(b&1) return a*binaryPow(a,b-1,m)%m;//  if(b&1)执行速度快一点
     else{
-        LL mul = binaryPow(a,b/2,m);//�������binaryPow(a,b/2)*binaryPow(a,b/2)������������
+        LL mul = binaryPow(a,b/2,m);//如果返回binaryPow(a,b/2)*binaryPow(a,b/2)计算量会增大
         return mul*mul%m;
     }
 }
 
 /*
-1.�����ʼa>m ���뺯��ǰ����a��mȡģ��
-2.���m=1��ֱ���ں����ⲿ����Ϊ0��
+1.如果初始a>m 进入函数前就让a对m取模。
+2.如果m=1，直接在函数外部特判为0。
 */
 
-//�����ݵĵ���д�� a = a`1*a`2*a`4*a`2`k
+//快速幂的迭代写法 
+/*
+b可以表示任意二进制
+比如3^13 = 3^(1101) = 3^8*3^4*3^1
+
+*/
 LL binaryPow(LL a,LL b,LL m){
     LL ans = 1;
     while(b>0){
-        if(b&1) ans = ans*a%m;//��ans�ۻ�a
-        a = a*a%m;
-        b>>=1;
+        if(b&1) ans = ans*a%m; //如果当前位是1，则乘相应的base
+        a = a*a%m; //base翻倍
+        b>>=1;//处理下一个位置
     }
 
 
 }
 
 //two pointers
-//�������Ȿ�������е����ԣ�ʹ�������±�����н���ɨ�裨����ͬ��Ҳ���Է���
-//���кϲ�����
+//利用问题本身与序列的特性，使用两个下标对序列进行扫描（可以同向，也可以反向）
+//序列合并问题
 
 int merge(int A[],int B[],int C[],int n,int m){
     int i=0,j=0,index = 0;
